3a4b893ee5a40df07779f47f4e7b59ff46833e3e
tobinfekkes <tobin@camanoislandcoffee.com>
2015-12-15 18:57:58 -0500

    [+] Only registers CSS and JS when viewing admin pages

--- woocommmerce-marketing.php
+++ woocommmerce-marketing.php
@@ -10,15 +10,16 @@
 	define( 'WOOCOMMERCE_MARKETING_URL', __FILE__);
 	define( 'WOOCOMMERCE_MARKETING_PATH', untrailingslashit(plugin_dir_path(__FILE__)));
 	
-	function woocommerce_marketing_css_and_js() {
-		wp_register_style('woocommerce_marketing_css', plugins_url('css/admin.css',__FILE__ ));
-		wp_enqueue_style('woocommerce_marketing_css');
-		wp_register_script('woocommerce_marketing_js', plugins_url('js/admin.js',__FILE__ ));
-		wp_enqueue_script('woocommerce_marketing_js');
-	}
-
-	add_action( 'wp_enqueue_scripts','woocommerce_marketing_css_and_js');
-
+    if (is_admin()) {
+        function woocommerce_marketing_css_and_js() {
+            wp_register_style('woocommerce_marketing_css', plugins_url('css/admin.css',__FILE__ ));
+            wp_enqueue_style('woocommerce_marketing_css');
+            wp_register_script('woocommerce_marketing_js', plugins_url('js/admin.js',__FILE__ ));
+            wp_enqueue_script('woocommerce_marketing_js');
+        }
+    
+        add_action( 'wp_enqueue_scripts','woocommerce_marketing_css_and_js');
+    }
 
 	// call register settings function
 	add_action( 'admin_init', 'register_woo_marketing_settings' );
